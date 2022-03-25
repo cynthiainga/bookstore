7d96bd84b6366979f0e0f2d3521a7d5f13bf2fc1
Cynthia Inga <candyinga67@gmail.com>
2022-03-25 10:45:38 +0200

    Add a bookstore container

---
+++ src/components/BookstoreContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Books from './Books';
+import Form from './Form';
+
+const BookstoreContainer = () => (
+  <main className="main-container">
+    <Books />
+    <Form />
+  </main>
+);
+
+export default BookstoreContainer;
