be43f68e3a9116ebe644f801318cd6448ae723ed
Cynthia Inga <candyinga67@gmail.com>
2022-03-30 22:36:41 +0200

    Add a functionality for check status

--- src/components/categoriesRoute.js
+++ src/components/categoriesRoute.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
 const Categories = () => {
   const styling = {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    fontSize: '30px',
+    height: '100px',
+  };
+  const categories = useSelector((state) => state.categoriesReducer);
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(checkStatus());
   };
 
   return (
-    <div style={styling}>
-      <h1>Under Construction</h1>
-    </div>
+    <main style={styling}>
+      <button type="button" onClick={handleClick}>Check Status</button>
+      <h1>
+        <br />
+        <br />
+        <br />
+        {categories}
+      </h1>
+    </main>
   );
 };
 
