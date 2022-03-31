ddafcec6f970aab989fbb67fe8f4b0cd4e911609
Cynthia Inga <candyinga67@gmail.com>
2022-03-31 11:39:20 +0200

    Use the HashRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './css/index.css';
 import App from './App';
 
 ReactDom.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
